bd20e91cc033f04f7a860a000cbf722c25b4bc4b
Beckett Simmons <beckettsimmons@hotmail.com>
2013-09-12 14:33:37 +0000

    Some major files added.

--- responseGen.php
+++ responseGen.php
@@ -15,28 +15,38 @@ class UserData
 
 function getResponse ($stanza, $userDataList){
     $tempUserData = new UserData();
+    $response = "default response";
     
     // first find out if we alreqady have data on the user
     foreach($userDataList as $user){
         if(!is_null($user)){
-            if($user->clientID== $stanza->from){
+            if($user->clientID== $stanza->from){ // if we get a match then we want to update the user data and get a response.
                 $tempUserData = $user;
+                //echo "got here";
+                
+                switch ($tempUserData->responseNumber) {
+                    case 0:
+                        $response = "1";
+                        break;
+                    case 1:
+                        $response = "2";
+                        break;
+                    case 2:
+                        $response = "3";
+                        break;
+                }
+                $tempUserData->responseNumber+=1;
+                
+                $user = $tempUserData;
+    
+    
+    
+    
                 break;
             }
         }
     }
-    switch ($tempUserData->responseNumber) {
-        case 0:
-            $response = "1";
-            break;
-        case 1:
-            $response = "2";
-            break;
-        case 2:
-            $response = "3";
-            break;
-    }
-    $tempUserData->responseNumber+=1;
+    
     return $response;
 }
 ?>
