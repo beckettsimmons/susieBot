3d3651e5bb1ecc5236abb9d124633556a13ae1d3
Beckett Simmons <beckettsimmons@hotmail.com>
2014-01-22 20:16:52 -0400

    Added basic 'thought engine' functionality.\nThis implements a simple loops so that the bot isn't simply event trigger by an incoming message.

--- susieBotConsole.php
+++ susieBotConsole.php
@@ -9,16 +9,45 @@ $knowledgeBase = generateKnowledgeBase();
 class Stanza{
 	public $from = 11;
 }
-
 $userDataList = array();
 
-$input = "";
-while($input != "exit"){
-	$input = readline("You  : ");
-	$stanza = new Stanza();
-	$stanza->body = $input;
-	echo "Susie: " . $knowledgeBase->getResponse($stanza) . "\n";
+
+//lines for allowing the 'thought' loop
+stream_set_blocking(STDIN, false);
+$time = microtime(true);
+$line = '';
+$prompt = 'You: ';
+
+
+echo $prompt;
+//Starting a primative 'though engine'.
+while(true){
+	$c = fgetc(STDIN);
+
+	if (microtime(true) - $time > 5){
+		$time = microtime(true);
+	}
+
+	if ($c !== false){
+		if ($c != "\n")
+			$line .= $c;
+		else{
+			if ($line == "exit")
+				break;
+			else{
+				$stanza = new Stanza();
+				$stanza->body = $line;
+				echo "Susie: ". $knowledgeBase->getResponse($stanza)."\n";
+			}
+			echo $prompt;
+			$line = '';
+		}
+	}
+	sleep(1);
 }
+
+
+
 
 
 ?>
